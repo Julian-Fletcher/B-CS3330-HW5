b78b90295fa82c071c4d0ae5f07c6742d7661970
Ahmad <121472038+ARockEnnin@users.noreply.github.com>
2024-03-11 14:42:27 -0500

    Full implementation and testing of the 3 cooking strategies
    Each cooking method now initializes the cooking strategy if null,
    initializes the cookingPrice, and adds the cookingPrice to the
    totalPrice

--- HW5/src/cookingStrategies/MicrowaveCookingStrategy.java
+++ HW5/src/cookingStrategies/MicrowaveCookingStrategy.java
@@ -16,30 +16,76 @@ public class MicrowaveCookingStrategy implements ICookingStrategy{
 	public boolean cook(AbstractPizza pizza) 
 	{
 		double microwaveCookingPrice = 1;
+		String pizzaType;
 		if(pizza instanceof MargheritaPizza)
 		{
-			((MargheritaPizza) pizza).setCookingPrice(microwaveCookingPrice);
-			((MargheritaPizza) pizza).setCookingStrategy(new MicrowaveCookingStrategy());
-			return true;
+			pizzaType = "MargheritaPizza";
 		}
 		else if(pizza instanceof VegetarianPizza)
 		{
-			((VegetarianPizza) pizza).setCookingPrice(microwaveCookingPrice);
-			((VegetarianPizza) pizza).setCookingStrategy(new MicrowaveCookingStrategy());
-			return true;
+			pizzaType = "VegetarianPizza";
 		}
 		else if(pizza instanceof HawaiianPizza)
 		{
-			((HawaiianPizza) pizza).setCookingPrice(microwaveCookingPrice);
-			((HawaiianPizza) pizza).setCookingStrategy(new MicrowaveCookingStrategy());
-			return true;
+			pizzaType = "HawaiianPizza";
 		}
-		else if(pizza instanceof SupremePizza)
+		else
 		{
-			((SupremePizza) pizza).setCookingPrice(microwaveCookingPrice);
-			((SupremePizza) pizza).setCookingStrategy(new MicrowaveCookingStrategy());
-			return true;
+			pizzaType = "SupremePizza";
 		}
-		return false;
+		
+		switch(pizzaType)
+		{
+			case "MargheritaPizza":
+			{
+				((MargheritaPizza)pizza).setCookingPrice(microwaveCookingPrice); //setting cooking price
+				if(((MargheritaPizza)pizza).getCookingStrategy() == null) //checking if a cooking strategy is already present
+				{
+					((MargheritaPizza)pizza).setCookingStrategy(new MicrowaveCookingStrategy());
+				}
+				double currentMargPizzaPrice = ((MargheritaPizza) pizza).getTotalPrice(); //get original total for this pizza
+				((MargheritaPizza) pizza).setTotalPrice(currentMargPizzaPrice + microwaveCookingPrice); //added the cost of microwave cooking to the original total
+				return true; //upon completion return true
+			}
+			case "VegetarianPizza":
+			{
+				((VegetarianPizza)pizza).setCookingPrice(microwaveCookingPrice);
+				if(((VegetarianPizza)pizza).getCookingStrategy() == null)
+				{
+					((VegetarianPizza)pizza).setCookingStrategy(new MicrowaveCookingStrategy());
+				}
+				double currentVegPizzaPrice = ((VegetarianPizza) pizza).getTotalPrice(); 
+				((VegetarianPizza) pizza).setTotalPrice(currentVegPizzaPrice + microwaveCookingPrice);
+				return true;
+			}
+			case "HawaiianPizza":
+			{
+				((HawaiianPizza)pizza).setCookingPrice(microwaveCookingPrice);
+				if(((HawaiianPizza)pizza).getCookingStrategy() == null)
+				{
+					((HawaiianPizza)pizza).setCookingStrategy(new MicrowaveCookingStrategy());
+				}
+				double currentHawaPizzaPrice = ((HawaiianPizza) pizza).getTotalPrice(); 
+				((HawaiianPizza) pizza).setTotalPrice(currentHawaPizzaPrice + microwaveCookingPrice);
+				return true;
+
+			}
+			case "SupremePizza":
+			{
+				((SupremePizza)pizza).setCookingPrice(microwaveCookingPrice);
+				if(((SupremePizza)pizza).getCookingStrategy() == null)
+				{
+					((SupremePizza)pizza).setCookingStrategy(new MicrowaveCookingStrategy());
+				}
+				double currentSupPizzaPrice = ((SupremePizza) pizza).getTotalPrice(); 
+				((SupremePizza) pizza).setTotalPrice(currentSupPizzaPrice + microwaveCookingPrice);
+				return true;
+			}
+			default:
+			{
+				break;
+			}
+		}
+		return false; 
 	}
 }

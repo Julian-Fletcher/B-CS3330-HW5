63ceeed5d27274d75a533f8843c4551c95f12a7a
Braedennnnn <149883938+Braedennnnn@users.noreply.github.com>
2024-03-11 14:51:45 -0500

    Abstract Upgrade and changed factory

--- HW5/src/pizzaOrder/PizzaCookingFactory.java
+++ HW5/src/pizzaOrder/PizzaCookingFactory.java
@@ -1,5 +1,42 @@
 package pizzaOrder;
 
+import pizzaType.PizzaType;
+import pizzaType.AbstractPizza;
+import pizzaType.HawaiianPizza;
+import pizzaType.MargheritaPizza;
+import pizzaType.SupremePizza;
+import pizzaType.VegetarianPizza;
+
 public class PizzaCookingFactory {
 
+	private int orderIDCounter = 0;
+	
+	public AbstractPizza createPizza(PizzaType pizzaType) {
+		
+		
+		AbstractPizza newPizza;
+		newPizza = null;
+		
+		if(pizzaType == PizzaType.HAWAIIAN) {
+			newPizza = new HawaiianPizza();
+		}
+		if(pizzaType == PizzaType.MARGHERITA) {
+			newPizza = new MargheritaPizza();
+		}
+		if(pizzaType == PizzaType.SUPREME) {
+			newPizza = new SupremePizza();
+		}
+		if(pizzaType == PizzaType.VEGETARIAN) {
+			newPizza = new VegetarianPizza();
+		}
+		newPizza.setPizzaOrderID(orderIDCounter);
+		orderIDCounter += 1;
+		
+		
+		
+		
+		return newPizza;
+	}
+
 }
+
--- HW5/src/pizzaType/AbstractPizza.java
+++ HW5/src/pizzaType/AbstractPizza.java
@@ -1,9 +1,9 @@
 package pizzaType;
 
-//import java.util.List;
+import java.util.List;
 
 public abstract class AbstractPizza {
-	// protected List<Toppings> toppingList;
+	protected List<Toppings> toppingList;
 	protected double priceWithoutToppings;
 	protected double totalPrice;
 	protected int pizzaOrderID;
@@ -16,6 +16,58 @@ public abstract class AbstractPizza {
 	public AbstractPizza() {
 		
 	}
+
+
+	public double getPriceWithoutToppings() {
+		return priceWithoutToppings;
+	}
+
+
+	public void setPriceWithoutToppings(double priceWithoutToppings) {
+		this.priceWithoutToppings = priceWithoutToppings;
+	}
+
+
+	public double getTotalPrice() {
+		return totalPrice;
+	}
+
+
+	public void setTotalPrice(double totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+
+	public int getPizzaOrderID() {
+		return pizzaOrderID;
+	}
+
+
+	public void setPizzaOrderID(int pizzaOrderID) {
+		this.pizzaOrderID = pizzaOrderID;
+	}
+
+
+	public static int getOrderIDCounter() {
+		return orderIDCounter;
+	}
+
+
+	public static void setOrderIDCounter(int orderIDCounter) {
+		AbstractPizza.orderIDCounter = orderIDCounter;
+	}
+
+
+	public double getCookingPrice() {
+		return cookingPrice;
+	}
+
+
+	public void setCookingPrice(double cookingPrice) {
+		this.cookingPrice = cookingPrice;
+	}
+	
+	
 
 	// Getters and setters
 	
